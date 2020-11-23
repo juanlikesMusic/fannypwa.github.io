cbd6be074a79167305a816119924f038f16ae190
jp <meliodasjp123@gmail.com>
2020-11-23 00:45:40 -0600

    edite sw.js

--- sw.js
+++ sw.js
@@ -3,15 +3,15 @@
  * se reinstale. Normalmente se cambia el número en el nombre del caché cuando
  * cambia el contenido de los archivos. Espera 11 minutos después de hacer los
  * cambios en tu sitio, para depués actualizar este archivo. */
-const CACHE = "fannypwa-1.00";
+const CACHE = "dmppwa-1.00";
 
 /**  Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
-"cmp/mi-footer.js",
+  "cmp/mi-footer.js",
   "css/estilos.css",
   "img/icono256.png",
   "img/icono1024.png",
-  "img/2048.png",
+  "img/icono2048.png",
   "js/registraServiceWorker.js",
   "favicon.ico",
   "index.html",
